8288f2ec8db763e055367360fdca480633dca48b
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-07-03 01:17:57 +0200

    set project

---
+++ src/main/java/com/example/studentstravel/StudentsTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentstravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentstravel/StudentsTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentstravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
