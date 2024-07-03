8c12177740fc4ec933e5584236e5cf89fd50c344
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-07-03 07:56:54 +0200

    set controller

--- src/main/java/com/example/studentstravel/student/StudentController.java
+++ src/main/java/com/example/studentstravel/student/StudentController.java
@@ -1,4 +1,28 @@
 package com.example.studentstravel.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
+    private StudentService studentService;
+
+    @GetMapping
+    public List<StudentEntity> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/average-age")
+    public double getAverageAge() {
+        return studentService.getAverageAge();
+    }
+
+    @GetMapping("/average-commute-time")
+    public double getAverageCommuteTime() {
+        return studentService.getAverageCommuteTime();
+    }
 }
